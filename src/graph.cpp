179b662a69af17c83c1f0a39ed2e3f2115c70b9f
magofrays <vadimpozdeev2005@gmail.com>
2025-04-07 12:37:17 +0300

    compacted

--- src/graph.cpp
+++ src/graph.cpp
@@ -9,16 +9,15 @@ void Graph::create_random_graph(size_t elements, double probability)
     this->probability = probability;
     for (size_t id = 0; id != elements; id++)
     {
-        vertex cur_vertex(id, elements);
-        cur_vertex.id = id;
-        for (size_t n_id = 0; n_id != elements; n_id++)
+        vertex cur_vertex(elements, false);             // создаем вершину графа
+        for (size_t n_id = 0; n_id != elements; n_id++) // цикл по соседям
         {
-            if (should_generate(probability))
+            if (should_generate(probability)) // добавлять ли соседа
             {
-                cur_vertex.neightbors[n_id] = true;
+                cur_vertex[n_id] = true;
             }
         }
-        add_vertex(cur_vertex);
+        add_vertex(cur_vertex); // добавляем вершину с соседями в граф
     }
 }
 
@@ -28,7 +27,7 @@ void Graph::print_matrix()
     {
         for (size_t j = 0; j != elements; j++)
         {
-            if (graph[i].neightbors[j])
+            if (graph[i][j])
             {
                 std::cout << "1" << " ";
             }
--- src/headers/graph.h
+++ src/headers/graph.h
@@ -1,16 +1,6 @@
 #include <vector>
 
-struct vertex
-{
-    size_t id;
-    std::vector<bool> neightbors; // vector true/false
-    vertex(size_t id, std::vector<bool> neightbors) : id(id), neightbors(neightbors) {}
-    vertex(size_t id, size_t n_size)
-    {
-        this->id = id;
-        neightbors = std::vector<bool>(n_size, false);
-    }
-};
+using vertex = std::vector<bool>;
 
 struct Graph
 {
@@ -18,8 +8,6 @@ struct Graph
     double probability;        // probability for generation
     std::vector<vertex> graph; // list of nodes
     Graph() = default;
-
-    void add_vertex(size_t id, std::vector<bool> neightbors) { graph.push_back({id, neightbors}); }
 
     void add_vertex(vertex &v) { graph.push_back(v); }
 
